aaa67b149e87a9894550c166e5856f878e4c051f
austinesamuel <austinesamuel914@gmail.com>
2021-08-18 23:33:44 +0100

    our website interface

---
+++ scripts/app.js
@@ -0,0 +1,1 @@
+//all user interface or we use react cdn or angular
---
+++ scripts/app.php
@@ -0,0 +1,19 @@
+<?php
+trait PraiseTechsMethods{//
+public function contactUs(){
+  //send mail to all our devs
+}
+public function savePageVisitor($user=array()){
+//lets us know who visit our site 
+}
+}
+
+interface Routes{
+//... routes
+}
+
+interface OurClients{
+  public function varify();
+  public function createAccount();//we gonna mailer for our registered clients here
+  public function chatUsOnline();//we gonna build chatbot here or whatsApp chatBot api
+}

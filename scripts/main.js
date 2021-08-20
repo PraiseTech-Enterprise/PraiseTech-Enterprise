887d3d62762d2798c1d38048568356f47a7e841c
PraiseTech-Enterprise <praisetechentreprice@gmail.com>
2021-08-20 22:50:43 +0100

    ready for checkouts maybe i should  add scroll effects

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,27 @@
 ///uis
+let menOpen=false;
+function closeMenu(){
+  if(!detectMob()){
+    message("this method is for mobiles only")
+  }
+  $("#menuContent").animate({marginLeft:screen.width+($("#menuContent").width()+100)},400);
+  $("#menu").attr("class","fa fa-reorder");
+  menOpen=false
+}
+
+function openMenu(){
+  if(!detectMob()){
+    message("this method is for mobiles only")
+  }
+  menOpen=true;
+  $("#menuContent").animate({marginLeft:"0"},400);
+  $("#menu").attr("class","fa fa-times")
+}
+
 onload=()=>{
-  $("#aboutGroup img").css("height", $("#aboutGroup img").css("width"))
+  $("#aboutGroup img").css("height", $("#aboutGroup img").css("width"));
+function openNClose(){
+menOpen ? closeMenu():openMenu();
+}
+  $("#menuContent button,#menu").click(openNClose);
 }

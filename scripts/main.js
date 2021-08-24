b2125082afb43f87f094033bd4a71dbf9adede04
DevBash1 <ikorosamuel1@gmail.com>
2021-08-24 14:13:22 +0100

    few changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,55 @@
 ///uis
-let menOpen=false;
-let timeout=()=>1;
-function closeMenu(){
-  if(!detectMob()){
-    message("this method is for mobiles only")
-  }
-timeout=setTimeout(() => {
-  $("#menuContent").hide();
-}, 1000);
-  $("#menuContent").animate({marginLeft:screen.width+($("#menuContent").width()+100)},400);
-  $("#menu").attr("class","fa fa-reorder");
-  menOpen=false
+let menOpen = false;
+let timeout = ()=>1;
+function closeMenu() {
+    if (!detectMob()) {
+        message("this method is for mobiles only")
+    }
+    timeout = setTimeout(()=>{
+        $("#menuContent").hide();
+    }
+    , 1000);
+    $("#menuContent").animate({
+        marginLeft: screen.width + ($("#menuContent").width() + 100)
+    }, 400);
+    $("#menu").attr("class", "fa fa-reorder");
+    menOpen = false
 }
 
-function openMenu(){
-if(innerWidth>450)return message("error command");
-  $("#menuContent").show();
-  clearTimeout(timeout);
-  if(!detectMob()){
-    message("this method is for mobiles only")
-  }
-  menOpen=true;
-  $("#menuContent").animate({marginLeft:"0"},400);
-  $("#menu").attr("class","fa fa-times")
+function openMenu() {
+    if (innerWidth > 450)
+        return message("error command");
+    $("#menuContent").show();
+    clearTimeout(timeout);
+    if (!detectMob()) {
+        message("this method is for mobiles only")
+    }
+    menOpen = true;
+    $("#menuContent").animate({
+        marginLeft: "0"
+    }, 400);
+    $("#menu").attr("class", "fa fa-times")
 }
 
-onload=()=>{
-  $("#aboutGroup img").css("height", $("#aboutGroup img").css("width"));
-function openNClose(){
-menOpen ? closeMenu():openMenu();
-}
-  $("#menuContent button,#menu").click(openNClose);
+onload = ()=>{
+    $("#aboutGroup img").css("height", $("#aboutGroup img").css("width"));
+    function openNClose() {
+        menOpen ? closeMenu() : openMenu();
+    }
+    $("#menuContent button,#menu").click(openNClose);
 
-addEventListener("resize",()=>{
-  if(innerWidth>450){
-    $("#menuContent").hide();
-  }
-})
+    addEventListener("resize", ()=>{
+        if (innerWidth > 450) {
+            $("#menuContent").hide();
+        }
+    }
+    )
 }
+
+//Render slider component
+
+let slide = Boxer("#slider",slider).render();
+
+//CacheJs Init
+let cacher = new cache();
+cacher.init();
